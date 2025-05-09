5d494b0039ada4093ea499bad0e1ff0838aa9946
DanielNV002 <nonprivado@gmail.com>
2025-05-09 12:02:41 +0200

    Proyecto Finalizado + BBDD

---
+++ BBDD.sql
@@ -0,0 +1,8 @@
+-- DROP DATABASE IF EXISTS artefattodb;
+-- CREATE DATABASE artefattodb;
+USE artefattodb;
+
+SELECT * FROM usuarios;
+SELECT * FROM productos;
+SELECT * FROM categorias;
+SELECT * FROM compras;
